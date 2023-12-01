2a8f2e1b3792b4def9c2066facac223f51e76726
JS <84028625+JS-Trebesin@users.noreply.github.com>
2023-12-01 18:25:10 +0100

    Create save_load.py

---
+++ save_load.py
@@ -0,0 +1,15 @@
+bludistaci = {
+    "Božetěch": 1,
+    "Želmíra": 3,
+    "Andělín": 4,
+    "Hvězdoslava": 1,
+}
+
+
+def ulozit_data():
+    print("tohle je placeholder, můžeš mě smazat")
+
+
+def nactist_data():
+    print("tohle je placeholder, můžeš mě smazat")
+  
